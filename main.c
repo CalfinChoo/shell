93227e5ce5807ce9b8b16b65e049e5292f40e84d
Anthony Li <ali04@stuy.edu>
2019-12-03 20:00:47 -0500

    testing

--- main.c
+++ main.c
@@ -5,13 +5,34 @@
 #include <errno.h>
 #include <sys/wait.h>
 
-char ** parse_args( char * line );
+void parse_args(char * line, char *** args);
+void errcheck();
 
 int main() {
-  char input[256];
-  char *args[16][16];
+  char input[1024];
+  char args[8][8][16];
   while (1) {
-
+    printf("Command:\n");
+    fgets(input, 735, stdin);
+    if (input[strlen(input) - 1] == '\n') input[strlen(input) - 1] = '\0';
+    errcheck();
+    parse_args(input, args);
+    int i = 0;
+    while (args[i]){
+      int f = fork();
+      if (strcmp(args[i], exit) == 0){
+        printf("exiting\n\n");
+        return 0;
+      }
+      if (f){
+        int status = 0;
+        wait(status);
+      }
+      else{
+        execvp(args[i][0], args[i]);
+      }
+      errcheck();
+    }
   }
   return 0;
 }
@@ -19,7 +40,7 @@ int main() {
 void parse_args(char * line, char *** args) {
   int i = 0;
   while(line != NULL) {
-    args[i] = strsep(&line, ";");
+    args[i][0] = strsep(&line, ";");
     i++;
   }
   args[i] = NULL;
@@ -31,5 +52,13 @@ void parse_args(char * line, char *** args) {
       args[i][j] = strsep(&l, " ");
       j++;
     }
+    args[i][j] = NULL;
+  }
+}
+
+void errcheck(){
+  if (errno) {
+    printf("Error: %d - %s\n", errno, strerror(errno));
+    errno = 0;
   }
 }
